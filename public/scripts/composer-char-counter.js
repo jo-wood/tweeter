30d22f3c9c480b49262bd2f1750b06ec12da7fc0
Alex Curci <alex.curci@gmail.com.com>
2019-06-12 01:06:36 +0000

    Update of counter and tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,33 +1,29 @@
 $(document).ready(function() {
   console.log("document is ready");
-  let counter = 0;
 
   $("#new-tweet-input").on("input", function(event) {
     //console.log(event.originalEvent.data); //The this keyword is a reference to the button
     
-    let $textArea = $(this);
+    const $textArea = $(this);
     //console.log($textArea.val().length);
     
-    let $textLength = $textArea.val().length; 
-    let $remainingChar = 140 - $textLength  
+    const $textLength = $textArea.val().length; 
+    const $remainingChar = 140 - $textLength;
     console.log($remainingChar);
     
-    let charCounter = ($(this).siblings(".counter"))
+    const charCounter = $(this).siblings(".counter");
     
     // Update counter to include remaining character
     charCounter[0].innerHTML = $remainingChar;
     
-    // Change coutner to red when value is negative
+    // Change counter to red when value is negative
     if ($remainingChar < 0) {
-      charCounter.css("color", "red");
+      charCounter.addClass("red");
+    } else {
+      charCounter.removeClass("red"); 
     }
-  
+    // $remainingChar < 0
+    //   ? charCounter.addClass("red")
+    //   : charCounter.removeClass("red"); 
   });
-  
-
-
-
-
-
 });
-
