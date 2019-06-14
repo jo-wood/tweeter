b562f91a59cc8d4117262926e3748ba4dd138bfa
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-14 10:48:21 -0400

    :sparkles: tweet correctly posting to database and rendering to feed

--- server/index.js
+++ server/index.js
@@ -23,9 +23,6 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-
-
-
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
 // actual database it uses and see little to no changes elsewhere in the code
